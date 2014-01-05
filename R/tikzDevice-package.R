6358755caae8feedd19f145744cbce10a1e48179
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-01-05 20:14:24 +0100

    Bump version to 0.7.2

--- R/tikzDevice-package.R
+++ R/tikzDevice-package.R
@@ -12,8 +12,8 @@
 #' \tabular{ll}{
 #'   Package: \tab tikzDevice\cr
 #'   Type: \tab Package\cr
-#'   Version: \tab 0.7.1\cr
-#'   Date: \tab 2013-12-21\cr
+#'   Version: \tab 0.7.2\cr
+#'   Date: \tab 2014-01-05\cr
 #'   License: \tab GPL 2 or greater\cr
 #'   LazyLoad: \tab yes\cr
 #' }
