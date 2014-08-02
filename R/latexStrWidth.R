45d41b3960abfecff6926160a52a4312fb645319
Yihui Xie <xie@yihui.name>
2014-08-02 10:06:25 -0500

    fixes #64: use / for Windows paths in getMetricsFromLatex()

--- R/latexStrWidth.R
+++ R/latexStrWidth.R
@@ -255,8 +255,8 @@ function( TeXMetrics ){
 	# Create the TeX file in a temporary directory so
 	# it doesn't clutter anything.
 	texDir <- tempdir()
-	texLog <- file.path( texDir,'tikzStringWidthCalc.log' )
-	texFile <- file.path( texDir,'tikzStringWidthCalc.tex' )
+	texLog <- normalizePath(file.path(texDir, 'tikzStringWidthCalc.log'), '/')
+	texFile <- normalizePath(file.path(texDir, 'tikzStringWidthCalc.tex'), '/')
 
 	# Open the TeX file for writing.
 	texIn <- file( texFile, 'w')
